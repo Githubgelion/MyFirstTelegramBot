456d40c43757fe13f2863b2ebe2ec28c8aea20b9
Githubgelion <kolch.stakan@gmail.com>
2020-06-15 15:53:49 +0300

    добавлена возможность скачивать фотографи

--- MyFirstTelegramBot/Program.cs
+++ MyFirstTelegramBot/Program.cs
@@ -13,6 +13,10 @@ using Telegram.Bot.Types.ReplyMarkups;
 using Newtonsoft.Json;
 using System.Collections.Generic;
 using static MyFirstTelegramBot.WeatherConfiguration;
+using System.Drawing;
+using System.Security.Cryptography.X509Certificates;
+using File = System.IO.File;
+using Telegram.Bot.Requests;
 
 namespace MyFirstTelegramBot
 {
@@ -20,6 +24,7 @@ namespace MyFirstTelegramBot
     {
         private static TelegramBotClient Bot;
 
+        public bool a = true;
         public static async Task Main()
         {
 
@@ -29,54 +34,54 @@ namespace MyFirstTelegramBot
 
             var me = await Bot.GetMeAsync();
             Console.Title = me.Username;
-
             Bot.OnMessage += BotOnMessageReceived;
             Bot.OnMessageEdited += BotOnMessageReceived;
             Bot.OnCallbackQuery += BotOnCallbackQueryReceived;
             Bot.OnInlineQuery += BotOnInlineQueryReceived;
             Bot.OnInlineResultChosen += BotOnChosenInlineResultReceived;
             Bot.OnReceiveError += BotOnReceiveError;
-
             Bot.StartReceiving(Array.Empty<UpdateType>());
             Console.WriteLine($"Start listening for @{me.Username}");
-
             Console.ReadLine();
             Bot.StopReceiving();
         }
 
+
         private static async void BotOnMessageReceived(object sender, MessageEventArgs messageEventArgs)
         {
             var message = messageEventArgs.Message;
+
             if (message == null || message.Type != MessageType.Text)
                 return;
-
-            switch (message.Text.Split(' ').First())
+            else
             {
-                // Send inline keyboard
-                case "/inline@Shinji_bot":
-                    await SendInlineKeyboard(message);
-                    break;
+                switch (message.Text.Split(' ').First())
+                {
+                    // Send inline keyboard
+                    case "/inline@Shinji_bot":
+                        await SendInlineKeyboard(message);
+                        break;
+                    case "/EditSendImage@Shinji_bot":
+                        await editUserPhoto(message);
+                        break;
+                    // send a photo
+                    case "/photo@Shinji_bot":
+                        await SendDocument(message);
+                        break;
+                    // request location or contact
+                    case "/request@Shinji_bot":
+                        await RequestContactAndLocation(message);
+                        break;
+                    case "/weather@Shinji_bot":
+                        await SendWeatherMoscow(message);
+                        break;
+                    case "/help@Shinji_bot":
+                        await Usage(message);
+                        break;
+                }
 
-                // send custom keyboard
-                //case "/keyboard":
-                //    await SendReplyKeyboard(message);
-                //    break;
-
-                // send a photo
-                case "/photo@Shinji_bot":
-                    await SendDocument(message);
-                    break;
-                // request location or contact
-                case "/request@Shinji_bot":
-                    await RequestContactAndLocation(message);
-                    break;
-                case "/weather@Shinji_bot":
-                    await SendWeatherMoscow(message);
-                    break;
-                case "/help@Shinji_bot":
-                    await Usage(message);
-                    break;
             }
+
 
             // Send inline keyboard
             // You can process responses in BotOnCallbackQueryReceived handler
@@ -109,7 +114,52 @@ namespace MyFirstTelegramBot
             }
 
 
-            
+            static async Task editUserPhoto(Message message)
+            {
+
+                await Bot.SendTextMessageAsync(chatId: message.Chat.Id, text: "Send some image.");
+                Bot.OnUpdate += BotOnMessageUpdate;
+                static async void BotOnMessageUpdate(object sender, UpdateEventArgs updateEventArgs)
+                {
+
+                    var message = updateEventArgs.Update.Message;
+                    if (message == null || message.Type != MessageType.Photo)
+                        return;
+                    else
+                        await SendEditImage(message);
+                    static async Task SendEditImage(Message message)
+                    {
+                        try
+                        {
+                            await Bot.SendChatActionAsync(message.Chat.Id, ChatAction.UploadPhoto);
+                            if (message == null || message.Type != MessageType.Text)
+                            {
+                                var file_info = await Bot.GetFileAsync(message.Photo.LastOrDefault().FileId);
+                                string fileName = file_info.FileId + "." + file_info.FilePath.Split('.').Last();
+
+                                using (var saveImageStream = File.Open(@"C:\ImageTelegram\" + fileName, FileMode.Create))
+                                {
+                                    await Bot.DownloadFileAsync(file_info.FilePath, saveImageStream);
+                                }
+                            }
+                            else
+                            {
+                                await Bot.SendTextMessageAsync(chatId: message.Chat.Id, text: "Error! Send some image.");
+                            }
+
+                            await Bot.SendTextMessageAsync(
+                                chatId: message.Chat.Id,
+                                text: "Downloaded"
+                            );
+                        }
+                        catch (Exception ex)
+                        {
+                            Console.WriteLine("Error downloading: " + ex.Message);
+                        }
+                    }
+                }
+            }
+
             //weather sender
             static async Task SendWeatherMoscow(Message message)
             {
@@ -123,31 +173,11 @@ namespace MyFirstTelegramBot
                 text: $"Temperature in Moscow: {Convert.ToString(dataWeather.t)} Apparent temperature: {Convert.ToString(dataWeather.aptmp)}");
             }
 
-
-            //static async Task SendReplyKeyboard(Message message)
-            //{
-            //    var replyKeyboardMarkup = new ReplyKeyboardMarkup(
-            //        new KeyboardButton[][]
-            //        {
-            //            new KeyboardButton[] { "Site AzurLane", "Weather in Moscow" },
-            //            new KeyboardButton[] { "Twitter"},
-            //        },
-            //        resizeKeyboard: true
-            //    );
-
-            //    await Bot.SendTextMessageAsync(
-            //        chatId: message.Chat.Id,
-            //        text: "Choose",
-            //        replyMarkup: replyKeyboardMarkup
-
-            //    );
-            //}
-            //image sender
             static async Task SendDocument(Message message)
             {
                 await Bot.SendChatActionAsync(message.Chat.Id, ChatAction.UploadPhoto);
 
-                const string filePath = @"Files/picture.jpg";
+                const string filePath = @"C:\Users\camic\Desktop\Study\C#\MyFirstTelegramBot\MyFirstTelegramBot\Shinji Zelenski.png";
                 using var fileStream = new FileStream(filePath, FileMode.Open, FileAccess.Read, FileShare.Read);
                 var fileName = filePath.Split(Path.DirectorySeparatorChar).Last();
                 await Bot.SendPhotoAsync(
@@ -156,8 +186,6 @@ namespace MyFirstTelegramBot
                     caption: "Nice Picture"
                 );
             }
-
-
 
             static async Task RequestContactAndLocation(Message message)
             {
@@ -172,8 +200,6 @@ namespace MyFirstTelegramBot
                     replyMarkup: RequestReplyKeyboard
                 );
             }
-
-
             //help
             static async Task Usage(Message message)
             {
